32453373896343ba001b4f82b0f621123d635d92
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-08-16 21:19:23 +0900

    3 files modified, 2 added

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+  return (
+    <main className="main">
+      <p>Sorry... Your Request's Page Not Found...</p>
+    </main>
+  );
+};
+
+export default Custom404;
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -4,8 +4,12 @@ export default function BlogId({ blog }) {
   return (
     <main>
       <h1>{blog.title}</h1>
-      <p>{blog.pubishedAt}</p>
-      <div dangerouslySetInnerHTML={{ __html: `${blog.body}` }} />
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
     </main>
   );
 }
@@ -14,14 +18,14 @@ export default function BlogId({ blog }) {
 export const getStaticPaths = async () => {
   const data = await client.get({ endpoint: 'blog' });
 
-  const paths = data.contents.map((content) => `blogs/${content.id}`);
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const data = await client.get({ endpoint: "blog", contentid: id });
+  const data = await client.get({ endpoint: 'blog', contentId: id });
 
   return {
     props: {
