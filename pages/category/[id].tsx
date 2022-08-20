2745dee9f1d1b13ee49ce0a4a11a0b78896d3951
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-08-21 01:54:08 +0900

    1 file modified

--- pages/category/[id].tsx
+++ pages/category/[id].tsx
@@ -21,3 +21,25 @@ export default function CategoryId({ blog }) {
     </div>
   );
 }
+
+// 静的生成のためのパスを指定
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'categories' });
+  const paths = data.contents.map((content) => `/category/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述する
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({
+    endpoint: 'blog',
+    queries: { filters: `category[equals]${id}` },
+  });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
