5fd8058cec6bcd680e41257fe56bf4faa78196a4
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-08-25 05:32:36 +0900

    1 file modified

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,19 @@ import Header from '../component/Header';
 import Footer from '../component/Footer';
 
 export default function About() {
+  const AboutParam = (name: string) => {
+    return <h3>{name}ってどんな人?</h3>;
+  };
   return (
     <div>
       <Header />
-
+      {AboutParam('Fill')}
+      <div className={styles.post}>
+        <h4></h4>
+        <h4></h4>
+        <h4></h4>
+        <h4></h4>
+      </div>
       <Footer />
     </div>
   );
