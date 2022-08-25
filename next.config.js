fe7e98503a7fbce12bed741baa524d73075ce794
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-08-25 15:15:43 +0900

    4 files modified

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const {createVanillaExtractPlugin} = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {}
+
 module.exports = {
   images: {
     domains: ['pbs.twimg.com'],
   },
-};
+  withVanillaExtract(nextConfig);
+}
