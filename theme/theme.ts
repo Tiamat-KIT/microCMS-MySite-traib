7ca6cf9161bd9f802a5bc9540225dc532196a593
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2022-08-25 14:23:02 +0900

    2 files modified

---
+++ theme/theme.ts
@@ -0,0 +1,11 @@
+import { fonts } from './setting/fonts';
+import { zindex } from './setting/zindex';
+import { colors } from './setting/colors';
+import { breakpoint } from './setting/breakpoint';
+
+export const theme = {
+  breakpoint: breakpoint,
+  colors: colors,
+  zindex: zindex,
+  fonts: fonts,
+};
